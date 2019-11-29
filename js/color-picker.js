5fe4c4cd9857914fffc76f1737a13a6163a2a937
Githubpoozas <po.oz.as@hotmail.com>
2019-11-29 16:55:45 +0700

    finish change color

--- js/color-picker.js
+++ js/color-picker.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $(".color-picker").click(function() {
-      var color = $(this).css("background-color");
-      var color2 = $(".column-front").css("background-color");
-      $(".column-front").css("background-color",color);
-      $(".column-back").css("background-color",color2);
+      var image1 = $(this).css("background-image");
+      var image2 = $(this).css("background-image").replace("_1", "_2");
+      $(".column-front").css("background-image",image1);
+      $(".column-back").css("background-image",image2);
     });
   });
   
