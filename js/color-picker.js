6851c6357549fe30cbeae643e91ebafa80a77f67
Githubpoozas <po.oz.as@hotmail.com>
2019-12-16 03:16:30 +0700

    convert color-picker from boostrap grid to css grid

--- js/color-picker.js
+++ js/color-picker.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
       $(".column-front").css("background-image",image1);
       $(".column-back").css("background-image",image2);
     });
+
+    $(".item__color-picker").click(function() {
+      var image1 = $(this).css("background-image");
+      var image2 = $(this).css("background-image").replace("_1", "_2");
+      $(".item__front").css("background-image",image1);
+      $(".item__back").css("background-image",image2);
+
+    });
+
   });
   
